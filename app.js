12bbbfeb98d10d6fc86c15800028bb4b4b9971d1
kenkenm43 <kenkenm43@gmail.com>
2022-05-23 17:12:16 +0700

    UPDATE workflow

--- app.js
+++ app.js
@@ -9,40 +9,8 @@ const session = require('express-session')
 const errorHandler = require('./src/middlewares/error-handler')
 const db = require('./src/models')
 // routes
-const Role = db.role
-app.use(function(req, res, next) {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-        )
-        next()
-    })
-db.sequelize.sync();
-// db.sequelize.sync({force: true}).then(() => {
-//     console.log('Drop and Resync Db');
-//     initial();
-//   });
-// function initial() {
-//     Role.create({
-//         id: 1,
-//         name: "user"
-//     });
-    
-//     Role.create({
-//       id: 2,
-//       name: "moderator"
-//     });
-  
-//     Role.create({
-//       id: 3,
-//       name: "admin"
-//     });
-//   }
-app.use(morgan("dev"))
-
-const PORT = process.env.PORT || 5000
-
-app.use(cors())
+const PORT = process.env.NODE_DOCKER_PORT || 5000;
+app.use(cors({ origin: '*' }))
 app.use(cookieParser())
 app.use(session({
     secret: 'secret',
@@ -52,6 +20,15 @@ app.use(session({
 // Body parse middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+        )
+        next()
+    })
+db.sequelize.sync();
+app.use(morgan("dev"))
 // global error handler
 app.use(errorHandler)
 
