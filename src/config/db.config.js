f5b6b805eac7a739268b86e515136988f707ee0d
kenkenm43 <kenkenm43@gmail.com>
2022-05-23 17:44:30 +0700

    UPDATE workflow

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,8 @@ module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB,
+    DB: process.env.DBNAME,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     pool: {
       max: 5,
