e68d35c250a2b8b3aa8744b18943beaa80eb1e8e
kenkenm43 <kenkenm43@gmail.com>
2022-10-26 23:42:17 +0700

    update status

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -12,7 +12,7 @@ exports.signinGoogle = async (req,res) => {
     try {
         const {email} = req.body
         const student = await Student.findOne({ where:{ email: email }})
-        if (!student) res.status(400).send({message:"โปรดลงทะเบียนก่อนเพื่อเข้าสู่ระบบโดยใช้อีเมล"})
+        if (!student) return res.status(400).send({message:"โปรดลงทะเบียนก่อนเพื่อเข้าสู่ระบบโดยใช้อีเมล"})
         const token = jwt.sign({ id: student.id }, config.secret, { expiresIn: 86400 })
         const user = await Student.findOne({ where:{ id: student.id }, attributes: { exclude: 'password' } })
         res
